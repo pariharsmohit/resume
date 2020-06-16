405d3bd65c52cd40526dc233519d26a47e70d2c9
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-06-16 11:00:06 +0530

    source changed tp massets

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -9,6 +9,6 @@ export class HttpService {
   constructor(private httpClient: HttpClient) { }
 
   getResume() {
-    return this.httpClient.get('https://mohitsparihar.github.io/resume.json')
+    return this.httpClient.get('https://mohitsparihar.github.io/massets/resume.json')
   }
 }
