88d744d3c663228fe440c8b85f09cf595ce5fdf7
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-08-19 22:52:56 +0530

    React resume data using axios

--- react-resume/src/App.js
+++ react-resume/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import resume from './assets/resume';
+// import resume from './assets/resume';
 import Header from './components/header/header.component';
 import Objective from './components/objective/objective.component';
 import Summary from './components/summary/summary.component';
@@ -9,20 +9,45 @@ import Projects from './components/projects/projects.component';
 import Experience from './components/experience/experience.component';
 import PersonalProjects from './components/personal-project/personal-project.component';
 import Education from './components/education/education.component';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <section className="App" id="content">
-      <Header resume={resume} />
-      <Objective objective={resume.objective} />
-      <Summary summary={resume.executiveSummary} />
-      <Skills skills={resume.skills}/>
-      <Projects projects={resume.projects} />
-      <Experience experience={resume.experience} />
-      <PersonalProjects personalProjects={resume.personalProjects} />
-      <Education education={resume.education} />
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resume: null
+    };
+  }
+
+  componentDidMount(){
+    Axios.get('https://mspwrites.com/massets/resume.json').then((response)=>{
+      this.setState({
+        resume: response.data
+      });
+    })
+  }
+
+  render() {
+    if(!this.state.resume){
+      return (
+        <section></section>
+      )
+    }
+    return (
+      <section className="App" id="content">
+      <Header resume={this.state.resume} />
+      <Objective objective={this.state.resume.objective} />
+      <Summary summary={this.state.resume.executiveSummary} />
+      <Skills skills={this.state.resume.skills}/>
+      <Projects projects={this.state.resume.projects} />
+      <Experience experience={this.state.resume.experience} />
+      <PersonalProjects personalProjects={this.state.resume.personalProjects} />
+      <Education education={this.state.resume.education} />
     </section>
-  );
-}
+    );
+  }
+};
+
 
 export default App;
